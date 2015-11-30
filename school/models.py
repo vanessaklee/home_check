7b21edd651bf32b643c9157395f50aaf06786650
Vanessa Lee <vklee@perx.com>
2015-11-30 00:09:46 -0600

    migration is failing

--- school/models.py
+++ school/models.py
@@ -2,7 +2,7 @@ from django.db import models
 import datetime
 
 class Resource(models.Model):
-    title = models.CharField(max_length=255)
+    title = models.CharField(max_length=255, verbose_name=b'Name of the resource?')
     cover = models.ImageField(upload_to=None, max_length=100, blank=True, null=True)
 
 class Assignment(models.Model):
