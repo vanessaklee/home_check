cf8e3d721b27a9de28a1f8f2c425e89b0e1c062e
Vanessa Lee <vklee@perx.com>
2015-11-29 23:15:14 -0600

    update school model display datefield

--- school/models.py
+++ school/models.py
@@ -4,7 +4,7 @@ import datetime
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     goal = models.CharField(max_length=255)
-    display_date = models.DateTimeField(default=datetime.datetime.now().isoformat())
+    display_date = models.DateField(default=datetime.date.today)
     complete = models.BooleanField(default=False, verbose_name=b'Are you finished with this assignment?')
     time_spent = models.IntegerField(default=0)
 
