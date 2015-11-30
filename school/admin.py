93d3b9c5334f7186f0902ccfc8fcc46f0d1b320c
Vanessa Lee <vklee@perx.com>
2015-11-29 22:43:17 -0600

    add school models

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from school.models import Assignment
 
-# Register your models here.
+class AssignmentAdmin(admin.ModelAdmin):
+    fields = ['title', 'display_date', 'goal', 'complete', 'time_spent']
+
+admin.site.register(Assignment, AssignmentAdmin)
--- school/models.py
+++ school/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Assignment(models.Model):
+    title = models.CharField(max_length=200)
+    goal = models.CharField(max_length=255)
+    display_date = models.DateTimeField('display date')
+    complete = models.BooleanField(default=False, verbose_name=b'Are you finished with this assignment?')
+    time_spent = models.IntegerField(default=0)
+
+
