a63beeb803a94a39acc45684708d5038f6731933
vanlee14 <vanessaklee@gmail.com>
2015-11-29 20:34:17 -0800

    initialze site

---
+++ home_check/settings_local.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'girlsleuth',
+        'USER': 'cherry_ames',
+        'PASSWORD': 'stud3ntnur$e1943',
+        'HOST': 'mysql.girlsleuth.com',
+        'PORT': '3306',
+    }
+}
