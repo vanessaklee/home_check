d2473edcecb66abea6013e785a8081a02a0049db
Vanessa Lee <vklee@perx.com>
2015-11-29 23:23:29 -0600

    allow some fields in assignment to be blank

--- school/models.py
+++ school/models.py
@@ -3,9 +3,9 @@ import datetime
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
-    goal = models.CharField(max_length=255)
+    goal = models.CharField(max_length=255, blank=True)
     display_date = models.DateField(default=datetime.date.today)
     complete = models.BooleanField(default=False, verbose_name=b'Are you finished with this assignment?')
-    time_spent = models.TimeField()
+    time_spent = models.TimeField(blank=True)
 
 
