9f7f4d033aa5e29c3b3895a54ecce6620361e9f3
Vanessa Lee <vklee@perx.com>
2015-11-30 00:07:08 -0600

    experimenting with nullable fields

--- school/models.py
+++ school/models.py
@@ -3,18 +3,18 @@ import datetime
 
 class Resource(models.Model):
     title = models.CharField(max_length=255)
-    cover = models.ImageField(upload_to=None, max_length=100)
+    cover = models.ImageField(upload_to=None, max_length=100, blank=True, null=True)
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     instructions = models.TextField(max_length=1000, blank=True)
-    resource = models.ForeignKey(Resource, blank=True)
-    website = models.URLField(max_length=200, blank=True)
-    goal = models.TextField(max_length=1000, blank=True)
+    resource = models.ForeignKey(Resource, blank=True, null=True)
+    website = models.URLField(max_length=200, blank=True, null=True)
+    goal = models.TextField(max_length=1000, blank=True, null=True)
     display_date = models.DateField(default=datetime.date.today)
     due_date = models.DateField(default=datetime.date.today)
     complete = models.BooleanField(default=False, verbose_name=b'Are you finished with this assignment?')
-    time_spent = models.TimeField(blank=True)
+    time_spent = models.TimeField(blank=True, null=True)
 
     def __str__(self):
       return self.title
