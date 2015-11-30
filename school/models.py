7f00af443290c54d92d874cabe5c5588a8524b0d
Vanessa Lee <vklee@perx.com>
2015-11-29 23:09:40 -0600

    update school model display date

--- school/models.py
+++ school/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+import datetime
 
 class Assignment(models.Model):
     title = models.CharField(max_length=200)
     goal = models.CharField(max_length=255)
-    display_date = models.DateTimeField('display date')
+    display_date = models.DateTimeField(default=datetime.datetime.now().isoformat())
     complete = models.BooleanField(default=False, verbose_name=b'Are you finished with this assignment?')
     time_spent = models.IntegerField(default=0)
 
